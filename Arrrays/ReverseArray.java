66707036bd3045c20e30bbae2d9279c7f070c1e2
Chetan Singh <chetan1.singh@airtel.com>
2022-03-23 18:09:07 +0530

    Write a program to reverse an array or string

--- Arrrays/ReverseArray.java
+++ Arrrays/ReverseArray.java
@@ -1,2 +1,30 @@
-package Arrrays;public class ReverseArray {
+//Write a program to reverse an array or string
+//complexity = O(n)
+
+package Arrrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        revArray(arr, n);
+        printArray(arr, n);
+    }
+
+    public static void revArray(int[] arr, int size) {
+        int temp;
+        int end = size - 1;
+        for (int i = 0; i < size - 1; i++) {
+            temp = arr[i];
+            arr[i] = arr[end];
+            arr[end] = temp;
+            end = end - 1;
+        }
+    }
+
+    public static void printArray(int[] arr, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
