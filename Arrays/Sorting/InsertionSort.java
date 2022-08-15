894ce6b6a4827ca64e08d88abe201de853d3f39b
Chetan Singh <chetan1.singh@airtel.com>
2022-08-15 12:38:23 +0530

    insertion sort

--- Arrays/Sorting/InsertionSort.java
+++ Arrays/Sorting/InsertionSort.java
@@ -2,24 +2,22 @@ package Arrays.Sorting;
 
 public class InsertionSort {
     public static void main(String[] args) {
-//        int[] arr = {3, 4, 7, 1, 2, 5, 6, 11, 9, 10};
-        int[] arr = {3, 1, 4,2};
+        int[] arr = {3, 4, 7, 1, 2, 5, 6, 11, 9, 10};
         int n = arr.length;
         insertionSort(arr, n);
         printArray(arr, n);
     }
 
     static void insertionSort(int[] arr, int n) {
-        for(int i = 1; i<n; i++){
+        for(int i = 1; i< n ; i++){
             int currentValue = arr[i];
             int j = i-1;
             while(j>=0 && arr[j]>currentValue){
                 arr[j+1] = arr[j];
-                j= j-1;
+                j--;
             }
-            arr[j+1] = currentValue;
+            arr[j+1] =currentValue;
         }
-
     }
 
     static void printArray(int[] arr, int n) {
