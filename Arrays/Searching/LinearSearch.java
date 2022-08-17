25d64b4db297297baed5e0c5997579a21b9f08e6
Chetan Singh <chetan1.singh@airtel.com>
2022-08-17 19:00:05 +0530

    linear search

--- Arrays/Searching/LinearSearch.java
+++ Arrays/Searching/LinearSearch.java
@@ -1,2 +1,17 @@
-package Arrays.Searching;public class LinearSearch {
+package Arrays.Searching;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 7, 1, 2, 5, 6, 11, 9, 10};
+        int ele = 9;
+        linearSearch(arr, ele);
+    }
+    static void linearSearch(int[] arr, int ele){
+        for(int i =0 ; i<arr.length;i++){
+            if (arr[i] == ele){
+                System.out.println("ele found at "+i+" index");
+                break;
+            }
+        }
+    }
 }
